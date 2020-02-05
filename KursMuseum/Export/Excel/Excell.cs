aae2142bc8003f926e2139a4aa77ddaf5aa993df
maksimyudin31@gmail.com <maksimyudin31@gmail.com>
2020-02-05 20:38:30 +0300

    Version 9.x

---
+++ KursMuseum/Export/Excel/Excell.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Office.Interop.Excel;
+
+namespace KursMuseum.Export.Excel
+{
+    public class Excell
+    {
+        protected Application app = new Application();
+        protected Workbook wb;
+        protected Worksheet ws;
+
+        protected string patternPath;
+
+        public Excell()
+        {
+
+        }
+    }
+}
---
+++ KursMuseum/Export/Pattern/Pattern.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KursMuseum.Export.Pattern
+{
+    public static class Pattern
+    {
+        public static class Doc
+        {
+            public static string ReportWord = @"E:\Учеба\KursProjectTheMuseum-master\KursMuseum\Export\Word\Report.xltx";
+        }
+        public static class Excel
+        {
+            public static string ReportExcel = @"E:\Учеба\KursProjectTheMuseum-master\KursMuseum\Export\Excel\Report.xltx";
+        }
+    }
+}
